e975764a491bc79bdf4ebb8577a7c0b930f0d793
Justin Henley <justin.a.henley@gmail.com>
2022-05-03 01:23:01 +0800

    menus route created

---
+++ routes/api/menus.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const menusController = require('../../controllers/menusController');
+
+router
+  .route('/')
+  .get(menusController.getAllMenus)
+  .post(menusController.createNewMenu)
+  .put(menusController.updateMenu)
+  .delete(menusController.deleteMenu);
+
+router.route('/:id').get(menusController.getMenu);
+
+module.exports = router;
