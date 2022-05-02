4f5b9ed454f17bf6cd28a55eb4dc5bfcf8e1c78f
Justin Henley <justin.a.henley@gmail.com>
2022-05-03 01:21:14 +0800

    server file written for basic json functionality

---
+++ server.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+const cors = require(cors);
+const connectDB = require('./config/dbConn');
+const PORT = process.env.PORT || 3500;
+
+// Connect to MongoDb
+connectDB();
+
+// Use CORS - Currently allowing all origins
+app.use(cors());
+
+// Middleware to handle url-encoded form data
+app.use(express.urlencoded({ extended: false }));
+
+// Middleware for JSON
+app.use(express.json());
+
+// static files?
+
+// routes
+app.use('/', require('./routes/root'));
+
+// api endpoints
+app.use('/dishes', require('./routes/api/dishes'));
+app.use('/menus', require('./routes/api/menus'));
+app.use('/restaurants', require('./routes/api/restaurants'));
+
+// Universal 404 page
+app.all('*', (req, res) => {
+  // Set response code
+  res.status(404);
+  // Send response as JSON
+  res.json({ error: '404 Not Found' });
+  // TODO an HTML version
+});
+
+// Announce successful database connection in console
+mongoose.connection.once('open', () => {
+  console.log('Connected to MongoDB');
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
