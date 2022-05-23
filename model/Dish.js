064738191f549633df0fc9e5dc253899ce69768d
Justin Henley <justin.a.henley@gmail.com>
2022-05-23 23:46:24 +0800

    Added a pinyin field without diacritics for easier searching

--- model/Dish.js
+++ model/Dish.js
@@ -6,10 +6,12 @@ const dishSchema = new Schema({
   zhtw: {
     type: String,
     required: true,
+    set: (x) => x.toLowerCase(),
   },
   pinyin: {
     type: String,
     required: true,
+    set: (x) => x.toLowerCase(),
   },
   meat: {
     type: String,
@@ -35,7 +37,22 @@ const dishSchema = new Schema({
   },
   en: {
     type: String,
+    set: (x) => x.toLowerCase(),
   },
+  // Should not be set manually
+  // The pre-save middleware strips the diacritics from the pinyin for easier searching
+  pinyinNoDiacritics: {
+    type: String,
+    default: '',
+  },
+});
+
+dishSchema.pre('save', function (next) {
+  this.pinyinNoDiacritics = this.pinyin
+    .normalize('NFD')
+    .replace(/\p{Diacritic}/gu, '');
+
+  next();
 });
 
 module.exports = mongoose.model('Dish', dishSchema);
