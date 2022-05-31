b2c2311a4fadf372e475b3cfdca179d0ac002e42
Justin Henley <justin.a.henley@gmail.com>
2022-05-31 15:45:51 +0800

    Updated menu schema

--- model/Menu.js
+++ model/Menu.js
@@ -3,15 +3,44 @@ const Schema = mongoose.Schema;
 const { ObjectID } = Schema.Types;
 
 const menuSchema = new Schema({
-  zhtw: String,
-  taigi: String,
-  en: String,
+  restaurant: {
+    zhtw: {
+      type: String,
+      required: true,
+      set: (x) => x.toLowerCase(),
+    },
+    pinyin: {
+      type: String,
+      required: true,
+      set: (x) => x.toLowerCase(),
+    },
+    en: {
+      type: String,
+      required: true,
+      set: (x) => x.toLowerCase(),
+    },
+    // Should not be set manually
+    // The pre-save middleware strips the diacritics from the pinyin for easier searching
+    pinyinNoDiacritics: {
+      type: String,
+      default: '',
+    },
+  },
   menu: [
     {
       type: ObjectID,
       ref: 'Dish',
     },
   ],
+});
+
+menuSchema.pre('save', function (next) {
+  this.restaurant.pinyinNoDiacritics = this.restaurant.pinyin
+    .normalize('NFD')
+    .replace(/\p{Diacritic}/gu, '')
+    .replace(/\s/g, '');
+
+  next();
 });
 
 module.exports = mongoose.model('Menu', menuSchema);
