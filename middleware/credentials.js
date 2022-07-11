2a73bb61dcfca05309ba0bf8ce135e1da46b75d8
Justin Henley <justin.a.henley@gmail.com>
2022-07-12 06:07:25 +0800

    header allow origin now available

--- middleware/credentials.js
+++ middleware/credentials.js
@@ -3,8 +3,12 @@ const allowedOrigins = require('../config/allowedOrigins');
 const credentials = (req, res, next) => {
   const origin = req.headers.origin;
 
+  // Credentials if provided
   if (allowedOrigins.includes(origin)) {
     res.header('Access-Control-Allow-Credential', true);
+  } else {
+    // Allow all origins for non-credentialed requests
+    res.header('Access-Control-Allow-Origin', '*');
   }
 
   next();
