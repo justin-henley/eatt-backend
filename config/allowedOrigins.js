94f85ded06c675bf55434e681097383ae913ec1e
Justin Henley <justin.a.henley@gmail.com>
2022-07-15 01:55:43 +0800

    Wrong urls in origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
-  'https://eatt-backend-test.herokuapp.com/dishes',
-  'https://eatt-backend-prod.herokuapp.com/dishes',
+  'https://eatt-frontend-test.herokuapp.com/dishes',
+  'https://eatt-frontend-prod.herokuapp.com/dishes',
   'http://localhost:3500',
   'http://localhost:3000',
 ];
