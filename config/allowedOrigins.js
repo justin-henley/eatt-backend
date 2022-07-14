3b002e2f2f956cb7529c714db2476bf2913be869
Justin Henley <justin.a.henley@gmail.com>
2022-07-15 02:00:41 +0800

    Origins fixed

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
-  'https://eatt-frontend-test.herokuapp.com/dishes',
-  'https://eatt-frontend-prod.herokuapp.com/dishes',
+  'https://eatt-frontend-test.herokuapp.com/',
+  'https://eatt-frontend-prod.herokuapp.com/',
   'http://localhost:3500',
   'http://localhost:3000',
 ];
