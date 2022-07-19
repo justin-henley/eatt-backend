db1dd60e4c19a311f5dbbdc483c1ffa45b89127d
Justin Henley <justin.a.henley@gmail.com>
2022-07-19 16:41:12 +0800

    Comments

--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -9,6 +9,7 @@ const handleLogout = async (req, res) => {
   if (!cookies?.jwt) return res.sendStatus(204); // No content
 
   const refreshToken = cookies.jwt;
+
   // Check if refresh token exists for some user
   const foundUser = await User.findOne({ refreshToken: refreshToken }).select('username refreshToken ').exec();
 
@@ -20,7 +21,7 @@ const handleLogout = async (req, res) => {
   }
 
   // Clear the cookie
-  res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true }); // Uncomment for local development with postman/thunderclient
+  res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true }); // Comment out 'secure:true' for local development with postman/thunderclient
   res.sendStatus(204); // No content
 };
 
