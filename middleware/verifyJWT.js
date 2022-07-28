3a52b2d55ae05e8e418c432a1a2e2afabde13fcd
Justin Henley <justin.a.henley@gmail.com>
2022-07-28 19:06:42 +0800

    dishes route now takes credentials for creation

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -5,12 +5,11 @@ const jwt = require('jsonwebtoken');
 
 const verifyJWT = (req, res, next) => {
   // Get the auth header
-  console.log('hello');
   const authHeader = req.headers.authorization || request.headers.Authorization;
-  console.log('auth headers: ', authHeader);
+
   // Ensure an auth header exists with a bearer token
   if (!authHeader?.startsWith('Bearer ')) return res.sendStatus(401); // Unauthorized
-
+  // Split off the word 'Bearer' and take just the token string
   const token = authHeader.split(' ')[1];
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
