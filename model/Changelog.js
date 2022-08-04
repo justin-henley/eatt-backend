f728f1c4a9ebfe77bdbd97918e7f32b4a54100f1
Justin Henley <justin.a.henley@gmail.com>
2022-08-04 19:03:41 +0800

    created changelog collection schema

---
+++ model/Changelog.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const { String, ObjectID, MongooseDate = Date } = Schema.Types;
+
+const changelogSchema = new Schema({
+  user: {
+    type: String,
+    required: true,
+  }, // user making changes
+  timestamp: {
+    type: MongooseDate,
+  }, // timestamp of change from backend
+  entryId: {
+    type: ObjectID,
+    required: true,
+  }, // id of the item changed
+  ref: {
+    type: String,
+    enum: ['Dish', 'Menu', 'Restaurant'],
+  }, // The collection the item is from
+  data: {
+    //what datatype to put here?
+  }, // the request body
+});
+
+// Set the timestamp
+changelogSchema.pre('save', function (next) {
+  this.timestamp = Date.now();
+
+  next();
+});
+
+module.exports = mongoose.model('Changelog', changelogSchema);
