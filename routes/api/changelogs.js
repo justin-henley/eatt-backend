6942c62da3fa0c2289d6f7b0493c599509f7c2b5
Justin Henley <justin.a.henley@gmail.com>
2022-08-05 00:11:05 +0800

    Created changelog route

---
+++ routes/api/changelogs.js
@@ -0,0 +1,17 @@
+// Libraries
+const express = require('express');
+const router = express.Router();
+// Controller
+const changelogsController = require('../../controllers/changelogsController');
+// Authentication
+const verifyJWT = require('../../middleware/verifyJWT');
+// Authorization
+const ROLES_LIST = require('../../config/roles_list');
+const verifyRoles = require('../../middleware/verifyRoles');
+
+// Other routes likely shouldn't be exposed. Create should only be internal to this backend, not exposed in the API. Changelogs should be immutable.
+
+// Authentication and authorization required for all paths
+router
+  .route('/')
+  .get(verifyJWT, verifyRoles(ROLES_LIST.Editor, ROLES_LIST.Admin), changelogsController.getAllChangelogs); // Get all changelogs
