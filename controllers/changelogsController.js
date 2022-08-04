7abcd041634e9884b0d991e958214c0e8e9e38db
Justin Henley <justin.a.henley@gmail.com>
2022-08-05 00:11:31 +0800

    Created changelog controller

---
+++ controllers/changelogsController.js
@@ -0,0 +1,23 @@
+// Libraries
+const mongoose = require('mongoose');
+// Models
+const Changelog = require('../model/Changelog');
+
+// Simplified Model
+// TODO add once stable
+
+// TODO Create
+const createChangelog = async (req, res) => {
+  // Check that all required parameters are provided
+  // Create the new log entry and return success
+  // Catch any errors and return failure to prevent the operation that generated the changelog
+  // TODO failure should halt the change
+};
+
+// TODO Read One
+
+// TODO Read all or range
+
+// TODO Update
+
+// TODO Delete
