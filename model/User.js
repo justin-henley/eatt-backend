b55e0158dd5b2d85ba096351d0a0d477c5be1585
Justin Henley <justin.a.henley@gmail.com>
2022-08-06 02:35:41 +0800

    Added email and confirmation to user schema

--- model/User.js
+++ model/User.js
@@ -1,14 +1,29 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const { String } = Schema.Types;
+const { String, Boolean } = Schema.Types;
 
 // TODO email, confirmed, plus add password recovery and email confirmations
 const userSchema = new Schema({
+  // TODO do NOT expose emails to any logs or API responses.
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  status: {
+    type: String,
+    enum: ['Pending', 'Active'], // Could expand to include 'Banned'
+    default: 'Pending',
+  },
+  confirmationCode: {
+    type: String,
+    unique: true,
+  },
   username: {
     type: String,
     required: true,
     unique: true,
-    set: (x) => x.toLowerCase(),
+    set: (x) => x.toLowerCase(), // TODO Should usernames be lowercase? Odd style choice, could just make login not case-sensitive for users
   },
   roles: {
     User: { type: Number, required: false, default: 2001 },
