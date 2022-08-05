111b4f797002f76f110adac2e1f4244b21b1e562
Justin Henley <justin.a.henley@gmail.com>
2022-08-05 17:41:34 +0800

    added get by id route

--- routes/api/changelogs.js
+++ routes/api/changelogs.js
@@ -10,10 +10,16 @@ const ROLES_LIST = require('../../config/roles_list');
 const verifyRoles = require('../../middleware/verifyRoles');
 
 // Other routes likely shouldn't be exposed. Create should only be internal to this backend, not exposed in the API. Changelogs should be immutable.
+// TODO add verfyJWT and verifyRoles to all requests in a single line
 
 // Authentication and authorization required for all paths
 router
   .route('/')
   .get(verifyJWT, verifyRoles(ROLES_LIST.Editor, ROLES_LIST.Admin), changelogsController.getAllChangelogs); // Get all changelogs
+
+// Returns a single changelog by id
+router
+  .route('/:id')
+  .get(verifyJWT, verifyRoles(ROLES_LIST.Editor, ROLES_LIST.Admin), changelogsController.getChangelog);
 
 module.exports = router;
