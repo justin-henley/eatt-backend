ccf31368c208a2fac84125bd421437e78602f981
Justin Henley <justin.a.henley@gmail.com>
2022-08-05 18:24:12 +0800

    Added logging middleware

--- routes/api/dishes.js
+++ routes/api/dishes.js
@@ -1,19 +1,24 @@
+// Libraries
 const express = require('express');
 const router = express.Router();
 const dishesController = require('../../controllers/dishesController');
+
+// MIDDLEWARE
 // Authentication
 const verifyJWT = require('../../middleware/verifyJWT');
 // Authorization
 const ROLES_LIST = require('../../config/roles_list');
 const verifyRoles = require('../../middleware/verifyRoles');
+// Logging
+const logChanges = require('../../middleware/logChanges');
 
 // Authentication and authorization required for post, patch, and delete
 router
   .route('/')
   .get(dishesController.getAllDishes) // Anyone for now, but restrict if heavy usage and large database
   .post(verifyJWT, verifyRoles(ROLES_LIST.Editor, ROLES_LIST.Admin), dishesController.createNewDish) // Editor or Admin
-  .patch(verifyJWT, verifyRoles(ROLES_LIST.Editor, ROLES_LIST.Admin), dishesController.updateDish) // Editor or Admin
-  .delete(verifyJWT, verifyRoles(ROLES_LIST.Admin), dishesController.deleteDish); // Admin only
+  .patch(verifyJWT, verifyRoles(ROLES_LIST.Editor, ROLES_LIST.Admin), logChanges, dishesController.updateDish) // Editor or Admin
+  .delete(verifyJWT, verifyRoles(ROLES_LIST.Admin), logChanges, dishesController.deleteDish); // Admin only
 
 // Returns a single dish by id
 router.route('/:id').get(dishesController.getDish); // Anyone
--- routes/api/menus.js
+++ routes/api/menus.js
@@ -1,19 +1,24 @@
+// Libraries
 const express = require('express');
 const router = express.Router();
 const menusController = require('../../controllers/menusController');
+
+//MIDDLEWARE
 // Authentication
 const verifyJWT = require('../../middleware/verifyJWT');
 // Authorization
 const ROLES_LIST = require('../../config/roles_list');
 const verifyRoles = require('../../middleware/verifyRoles');
+// Logging
+const logChanges = require('../../middleware/logChanges');
 
 // Authentication and authorization required for post, patch, and delete
 router
   .route('/')
   .get(menusController.getAllMenus) // Anyone for now, but restrict if heavy usage and large database
   .post(verifyJWT, verifyRoles(ROLES_LIST.Editor, ROLES_LIST.Admin), menusController.createNewMenu) // Editor or Admin
-  .patch(verifyJWT, verifyRoles(ROLES_LIST.Editor, ROLES_LIST.Admin), menusController.updateMenu) // Editor or Admin
-  .delete(verifyJWT, verifyRoles(ROLES_LIST.Admin), menusController.deleteMenu); // Admin only
+  .patch(verifyJWT, verifyRoles(ROLES_LIST.Editor, ROLES_LIST.Admin), logChanges, menusController.updateMenu) // Editor or Admin
+  .delete(verifyJWT, verifyRoles(ROLES_LIST.Admin), logChanges, menusController.deleteMenu); // Admin only
 
 router.route('/:id').get(menusController.getMenu); // Anyone
 
