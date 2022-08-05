7da34df9518966a840a450898a73e17ea82d06c3
Justin Henley <justin.a.henley@gmail.com>
2022-08-06 02:35:10 +0800

    zdded confirmation code to user registration w/ email

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,11 +1,13 @@
 // Libraries
 const bcrypt = require('bcrypt');
+// Configured mailer
+const nodemailer = require('../config/nodemailer.config');
 // Models
 const User = require('../model/User');
 
 const handleNewUser = async (req, res) => {
   // Check if username and password were provided
-  const { user, pwd } = req.body;
+  const { user, pwd, email } = req.body;
   if (!user || !pwd) return res.status(400).json({ message: 'Username and password are required.' });
 
   // Check for duplicate usernames in database
@@ -14,15 +16,23 @@ const handleNewUser = async (req, res) => {
   try {
     // Encrypt the password
     const hashedPwd = await bcrypt.hash(pwd, 10);
-    // Create new user entry
 
+    // Create a confirmation code
+    const conf = jwt.sign({ email: email }, process.env.CONFIRM_CODE_SECRET);
+
+    // Create new user entry
     const result = await User.create({
       username: user,
       password: hashedPwd,
+      email: email,
+      confirmationCode: conf,
     });
 
+    // Request succeeded if it reaches this line, so send the email
+    nodemailer.sendConfirmationEmail(user, email, conf);
+
     // Respond with success
-    res.status(201).json({ message: `New user ${user} created.` });
+    res.status(201).json({ message: `New user ${user} created. Please check your email for a confirmation link.` });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
