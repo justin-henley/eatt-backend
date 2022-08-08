488be6d7f3c01540c4e68930f762669db46f04a3
Justin Henley <justin.a.henley@gmail.com>
2022-08-08 23:05:16 +0800

    Decipher ref in changelog from url

--- middleware/logChanges.js
+++ middleware/logChanges.js
@@ -1,32 +1,40 @@
 // Libraries
 const mongoose = require('mongoose');
-const { sendStatus } = require('express/lib/response');
 // Models
 const Changelog = require('../model/Changelog');
-// TODO test and verify
 
 // Log any changes to documents in the DB
 const logChanges = async (req, res, next) => {
   // Check that all required parameters are provided
-  if (!req?.user || !req?.body?.id || !req?.body?.data) return res.sendStatus(400);
-  /* .json({ message: 'Username, entry ID, ref, and data fields required to generate changelog.' }); */
+  if (!req?.user || !req?.body?.id) {
+    return res.status(400).json({ message: 'Username, entry ID, and data fields required to generate changelog.' });
+  }
 
-  // TODO decipher ref from request
-  let ref;
+  // Decipher ref from request
+  let ref =
+    req.baseUrl === '/dishes'
+      ? 'Dish'
+      : req.baseUrl === '/menus'
+      ? 'Menu'
+      : req.baseUrl === '/restaurants'
+      ? 'Restaurant'
+      : 'None';
 
   // Create the new log entry and return success
   try {
     const result = await Changelog.create({
       user: req.user,
-      entryId: req.body.id,
-      ref: ref,
+      entry: {
+        id: req.body.id,
+        ref: ref,
+      },
       data: req.body,
     });
 
     next();
   } catch (error) {
     // Catch any errors and return failure to prevent the operation that generated the changelog
-    res.sendStatus(500); /* .json({ message: 'An unknown error occured during changelog creation.' }); */
+    res.status(500).json({ message: 'An unknown error occured during changelog creation.' });
   }
 };
 module.exports = logChanges;
