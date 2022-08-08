a64f5ab348c8b5ea1681c09abd7da4e777a3453e
Justin Henley <justin.a.henley@gmail.com>
2022-08-08 23:03:12 +0800

    Descriptive error messages

--- middleware/verifyRoles.js
+++ middleware/verifyRoles.js
@@ -1,5 +1,4 @@
 // Libraries
-const { sendStatus } = require('express/lib/response');
 
 const verifyRoles = (...allowedRoles) => {
   // Outer function allows passing in the allowed roles
@@ -10,7 +9,7 @@ const verifyRoles = (...allowedRoles) => {
 
     const result = req.roles.map((role) => rolesArray.includes(role)).find((val) => val === true);
 
-    if (!result) return res.sendStatus(401);
+    if (!result) return res.status(401).json({ message: 'Error: You do not have permission for this operation.' });
 
     next();
   };
