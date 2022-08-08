03c5e1bcf1ab6b824b6b490fdb2069a5c3bc4ed2
Justin Henley <justin.a.henley@gmail.com>
2022-08-08 23:03:20 +0800

    typo

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
 
 const verifyJWT = (req, res, next) => {
   // Get the auth header
-  const authHeader = req.headers.authorization || request.headers.Authorization;
+  const authHeader = req.headers.authorization || req.headers.Authorization;
 
   // Ensure an auth header exists with a bearer token
   if (!authHeader?.startsWith('Bearer ')) return res.sendStatus(401); // Unauthorized
