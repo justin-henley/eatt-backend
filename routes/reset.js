4bffaa74206414900013aed70928b312a8ee643a
Justin Henley <justin.a.henley@gmail.com>
2022-08-09 14:42:09 +0800

    Created reset token route

---
+++ routes/reset.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const resetController = require('../controllers/resetController');
+
+router.post('/', resetController.generateReset);
+router.post('/:resetToken', resetController.handleReset);
+
+module.exports = router;
