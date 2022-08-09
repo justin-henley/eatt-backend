d117af16f710a6a3fbd64cadf4d2a8e1e5254350
Justin Henley <justin.a.henley@gmail.com>
2022-08-09 16:55:27 +0800

    swtiched to requiring email, not userId

--- controllers/resetController.js
+++ controllers/resetController.js
@@ -10,25 +10,23 @@ const { resetTokenExpiresIn } = require('../config/constants');
 
 const generateReset = async (req, res) => {
   // Get userId from request
-  const { userId } = req.body;
-  if (!userId) return res.status(400).json({ message: 'UserId required' });
+  const { email } = req.body;
+
+  if (!email) return res.status(400).json({ message: 'User email address required' });
 
   // Check if that user exists
-  try {
-    const foundUser = await User.findById(userId).exec();
-    if (!foundUser) return res.status(400).json({ message: 'Invalid user ID' });
-  } catch (error) {
-    // Handles cating errors of userId to mongoose ObjectID type
-    return res.status(400).json({ message: 'Invalid user ID' });
-  }
+
+  const foundUser = await User.findOne({ email: email }).exec();
+  if (!foundUser) return res.status(400).json({ message: 'Invalid email address' });
+
   // Check if a reset token already exists for that user
-  const foundToken = await ResetToken.findOne({ userId: userId }).exec();
+  const foundToken = await ResetToken.findOne({ userId: foundUser._id }).exec();
   if (foundToken) return res.status(409).json({ message: 'Reset token already exists for this user.' });
 
   // Generate a reset token
   const resetToken = jwt.sign(
     {
-      userId: userId,
+      userId: foundUser._id,
     },
     process.env.RESET_TOKEN_SECRET,
     { expiresIn: '15m' }
@@ -37,12 +35,11 @@ const generateReset = async (req, res) => {
   try {
     // Store the reset token
     const storedToken = await ResetToken.create({
-      userId: userId,
+      userId: foundUser._id,
       token: resetToken,
     });
 
     // Send the reset token to the user's email
-    console.log(foundUser.username, foundUser.email, foundUser);
     sendPasswordResetEmail(foundUser.username, foundUser.email, resetToken);
 
     // Respond with success
@@ -57,17 +54,27 @@ const handleReset = async (req, res) => {
   const reqToken = { token: req.params.resetToken };
   jwt.verify(reqToken.token, process.env.RESET_TOKEN_SECRET, (err, decoded) => {
     // Error
-    if (err) return res.status(403).json('Invalid or expired token.');
-    // Get userId from token
-    reqToken.userId = decoded.userId;
+    if (err) {
+      reqToken.err = true;
+    } else {
+      // Get userId from token
+      reqToken.userId = decoded.userId;
+    }
   });
+
+  // Return if the token could not be decoded
+  if (reqToken.err) return res.status(403).json('Invalid or expired token.');
 
   try {
     // Search ResetTokens collection for this user with this token that has not expired
     const foundToken = await ResetToken.findOne({ userId: reqToken.userId });
     // Token not found
     if (!foundToken) return res.status(403).json({ message: 'Invalid or expired token.' });
+  } catch (error) {
+    return res.status(500).json({ message: 'Error: ' });
+  }
 
+  try {
     // If found, reset user password to the one in the request
     const user = await User.findById(foundToken.userId).exec(); // Find user
     const hashedNewPwd = await bcrypt.hash(req.body.pwd, 10); // Encrypt the password
@@ -80,7 +87,7 @@ const handleReset = async (req, res) => {
     // Announce success
     return res.status(200).json({ message: 'Password updated. You may now log in with your new password.' });
   } catch (error) {
-    return res.status(500).json({ message: `Error: ${error}` });
+    return res.status(500).json({ message: 'Error: ' });
   }
 };
 
