1d548bf887bb87984626316b19bc253e1e6dffe1
Justin Henley <justin.a.henley@gmail.com>
2022-08-11 17:43:16 +0800

    created account controller for retrieving user account data

---
+++ controllers/accountController.js
@@ -0,0 +1,39 @@
+// Libraries
+const mongoose = require('mongoose');
+// Models
+const Dish = require('../model/Dish');
+const Menu = require('../model/Menu');
+
+// Get all dishes for the logged in user account. Requires auth context
+const getUserDishes = async (req, res) => {
+  // Username is stored in req.username by verifyJWT middleware
+
+  // Get all dishes by the user
+  // TODO is that search query correct?
+  const dishes = await Dish.find({ history: { creator: req.username } });
+
+  // No data found
+  if (!dishes) {
+    return res.status(204).json({ message: "You haven't created any dishes yet." });
+  }
+
+  // Return data as json
+  res.json(dishes);
+};
+
+// Get all menus for the logged in user
+const getUserMenus = async (req, res) => {
+  // Username is stored in req.username by verifyJWT middleware
+
+  // Get all menus by the user
+  // TODO validate this search
+  const menus = await Menu.find({ history: { creator: req.username } });
+
+  // No data found
+  if (!menus) {
+    return res.status(204).json({ message: "You haven't created any menus yet." });
+  }
+
+  // Return data as json
+  res.json(menus);
+};
