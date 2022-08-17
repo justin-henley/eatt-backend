b1992af1f59f728718f1a9619633eadfc520280b
Justin Henley <justin.a.henley@gmail.com>
2022-08-17 16:56:34 +0800

    Read all is now admin-only

--- routes/api/menus.js
+++ routes/api/menus.js
@@ -16,11 +16,15 @@ const logChanges = require('../../middleware/logChanges');
 router
   // TODO find a way to protect the get all route without breaking the search function
   .route('/')
-  .get(menusController.getAllMenus) // Anyone for now, but restrict if heavy usage and large database
+  .get(menusController.searchMenus) // Anyone for now, but restrict if heavy usage and large database
   .post(verifyJWT, /* verifyRoles(ROLES_LIST.Editor, ROLES_LIST.Admin), */ menusController.createNewMenu) // Editor or Admin
   .patch(verifyJWT, /* verifyRoles(ROLES_LIST.Editor, ROLES_LIST.Admin), */ logChanges, menusController.updateMenu) // Editor or Admin
   .delete(verifyJWT, verifyRoles(ROLES_LIST.Admin), logChanges, menusController.deleteMenu); // Admin only
 
+// Get a single menu by ID
 router.route('/:id').get(menusController.getMenu); // Anyone
+
+// Retrieve all menus
+router.route('/all').get(verifyJWT, verifyRoles(ROLES_LIST.Admin), menusController.getAllMenus);
 
 module.exports = router;
