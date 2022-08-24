e84b19fece7f6fc1027c18efc4561654bdf2cbac
Justin Henley <justin.a.henley@gmail.com>
2022-08-24 21:36:00 +0800

    Removed unused code

--- middleware/credentials.js
+++ middleware/credentials.js
@@ -6,10 +6,6 @@ const credentials = (req, res, next) => {
   // Credentials if provided
   if (allowedOrigins.includes(origin)) {
     res.header('Access-Control-Allow-Credentials', true);
-    /* res.header('Access-Control-Allow-Origin', origin); */
-  } else {
-    // Allow all origins for non-credentialed requests
-    /* res.header('Access-Control-Allow-Origin', '*'); */
   }
 
   next();
