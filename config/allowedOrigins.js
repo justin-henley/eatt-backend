83fae67f6c6eab5d2951e32fe3251d3d786f8853
Justin Henley <justin.a.henley@gmail.com>
2022-08-27 00:41:28 +0800

    Explicitly listed allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ['http://localhost:3000', 'https://eatt-frontend-test.onrender.com/'];
+const allowedOrigins = ['http://localhost:3000', 'http://localhost:3500', 'https://eatt-frontend-test.onrender.com'];
 
 module.exports = allowedOrigins;
