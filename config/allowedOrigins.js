862f0fc3160fecfbfa5064edc69037d25ca00c20
Justin Henley <justin.a.henley@gmail.com>
2022-08-26 23:33:00 +0800

    Allowed origins come from env only

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,3 @@
-const allowedOrigins = [
-  'https://eatt-frontend-test.herokuapp.com',
-  'https://eatt-frontend-prod.herokuapp.com',
-  'http://localhost:3500',
-  'http://localhost:3000',
-];
+const allowedOrigins = [process.env.FRONTEND_URL];
 
 module.exports = allowedOrigins;
