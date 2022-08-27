5129ea202efff45f5232f30a16a66487b93f697f
Justin Henley <justin.a.henley@gmail.com>
2022-08-27 23:35:39 +0800

    Added production frontend to allow list

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,9 @@
-const allowedOrigins = ['http://localhost:3000', 'http://localhost:3500', 'https://eatt-frontend-test.onrender.com'];
+// TODO move to env
+const allowedOrigins = [
+  'http://localhost:3000',
+  'http://localhost:3500',
+  'https://eatt-frontend-test.onrender.com',
+  'https://eatt-frontend.onrender.com/',
+];
 
 module.exports = allowedOrigins;
