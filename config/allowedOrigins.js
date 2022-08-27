e84b18d40e869935f04df2d000b549445ae33e64
Justin Henley <justin.a.henley@gmail.com>
2022-08-28 00:36:22 +0800

    Fix production frontend url

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,7 +3,7 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:3500',
   'https://eatt-frontend-test.onrender.com',
-  'https://eatt-frontend.onrender.com/',
+  'https://eatt-frontend.onrender.com',
 ];
 
 module.exports = allowedOrigins;
