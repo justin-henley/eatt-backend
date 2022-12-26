92ac06598871b27e7391de10988d05bf6db005fc
Justin Henley <justin.a.henley@gmail.com>
2022-12-27 00:27:24 +0800

    Structure test folder

--- __tests__/routes/root.test.js
+++ __tests__/routes/root.test.js
@@ -1,32 +1,38 @@
 const request = require('supertest');
-const connectDB = require('../config/dbConn');
-const app = require('../app');
+const connectDB = require('../../config/dbConn');
+const app = require('../../app');
 const mongoose = require('mongoose/index.js');
 
 describe('Good index routes', () => {
-  test('responds to /', async () => {
+  test('GET / as html', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toBe(200);
   });
 
-  test('responds to /index', async () => {
+  test('GET /index', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toBe(200);
   });
 
-  test('responds to /index.html', async () => {
+  test('GET /index.html', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toBe(200);
+  });
+
+  test('GET / as json', async () => {
+    const res = await request(app).get('/').set({ Accept: 'application/json' });
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toEqual({ message: 'Welcome!' });
   });
 });
 
 describe('Good dish routes', () => {
-  beforeEach(() => {
-    connectDB();
+  beforeEach(async () => {
+    await connectDB();
   });
   test('responds to /dishes', async () => {
     const res = await request(app).get('/dishes?en=').set({ Accept: 'application/json' });
-    console.log(res.body);
+    // console.log(res.body);
     expect(res.statusCode).toBe(200);
   });
   afterEach((done) => {
